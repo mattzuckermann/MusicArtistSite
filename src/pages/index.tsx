03476e9059136269ea4b8eeabb64b4b536088b00
Matt Zuckermann <mczuckermann@gmail.com>
2019-04-04 22:02:04 -0700

    Adjusted depenecies, configs, and json to utilize typescript, eslint, and bootstrap within website

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,10 +5,15 @@ import Layout from '../components/Layouts/layout';
 import Image from '../components/Image';
 import SEO from '../components/SEO';
 import Archive from '../components/Archive';
+import 'bootstrap/dist/css/bootstrap.css';
 
 const IndexPage = () => (
   <Layout>
-    <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
+    <SEO
+      description="visibility improvement"
+      title="Home"
+      keywords={[`gatsby`, `application`, `react`]}
+    />
     <h1>Hey peeps...</h1>
     <p>Welcome to my music site.</p>
     <p>Enjoy the sweet tunes.</p>
