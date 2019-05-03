cee24045827201f355f6606c6c80545e4ef44f45
Matt Zuckermann <mczuckermann@gmail.com>
2019-05-02 21:12:12 -0700

    Created index files to reference other files in folder

---
+++ src/audio/index.ts
@@ -0,0 +1,21 @@
+/* eslint-disable import/no-unresolved */
+/* eslint-disable global-require */
+import singleImages from '../images/singles';
+
+export default [
+  {
+    name: 'Reiki',
+    src: require('./singles/Reiki.mp3'),
+    image: singleImages.reiki,
+  },
+  {
+    name: 'Crush',
+    src: require('./singles/Crush.mp3'),
+    image: singleImages.crush,
+  },
+  {
+    name: 'Two Weeks',
+    src: require('./singles/Two-Weeks.wav'),
+    image: singleImages.twoWeeks,
+  },
+];
