1c9a43fc54e1cea7148085f85576629ada95e128
Matt Zuckermann <mczuckermann@gmail.com>
2019-05-14 15:42:52 -0700

    Adjusted spelling of attribute for React

--- src/components/Layouts/postLayout.tsx
+++ src/components/Layouts/postLayout.tsx
@@ -104,7 +104,7 @@ const TextMobileStepper = () => {
                     width="528"
                     height="297"
                     src="https://www.youtube.com/embed/nIds3reW_dY?rel=0"
-                    frameborder="0"
+                    frameBorder="0"
                     allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture; fullscreen"
                   />
                 </div>
