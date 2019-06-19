8720f170e1e01885e1054640a3eb972bd213581f
Matt Zuckermann <mczuckermann@gmail.com>
2019-06-19 00:57:20 -0700

    re-added audio files to assets

---
+++ src/audio/index.ts
@@ -0,0 +1,14 @@
+/* eslint-disable import/no-unresolved */
+/* eslint-disable global-require */
+
+export default [
+  {
+    src: require('./singles/Reiki.mp3'),
+  },
+  {
+    src: require('./singles/Crush.mp3'),
+  },
+  {
+    src: require('./singles/Two-Weeks.wav'),
+  },
+];
