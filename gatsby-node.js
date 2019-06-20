7b969d2f07c30c55cb8839326e72dadfe9dd6f8d
Matt Zuckermann <mczuckermann@gmail.com>
2019-06-19 17:01:01 -0700

    Commented out gatsby-node.js's dynamically creating pages feature

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,31 +1,31 @@
-const path = require('path');
+// const path = require('path');
 
-const ALL_POSTS_QUERY = `{
-        allMarkdownRemark {
-          edges {
-            node {
-              frontmatter {
-                slug
-              }
-            }
-          }
-        }
-      }`;
+// const ALL_POSTS_QUERY = `{
+//         allMarkdownRemark {
+//           edges {
+//             node {
+//               frontmatter {
+//                 slug
+//               }
+//             }
+//           }
+//         }
+//       }`;
 
-exports.createPages = ({ graphql, actions }) => {
-  const { createPage } = actions;
-  return new Promise((resolve, reject) => {
-    graphql(ALL_POSTS_QUERY).then(results => {
-      results.data.allMarkdownRemark.edges.forEach(({ node }) => {
-        createPage({
-          path: `${node.frontmatter.slug}`,
-          component: path.resolve('./src/components/Layouts/postLayout.tsx'),
-          context: {
-            slug: node.frontmatter.slug,
-          },
-        });
-      });
-      resolve();
-    });
-  });
-};
+// exports.createPages = ({ graphql, actions }) => {
+//   const { createPage } = actions;
+//   return new Promise((resolve, reject) => {
+//     graphql(ALL_POSTS_QUERY).then(results => {
+//       results.data.allMarkdownRemark.edges.forEach(({ node }) => {
+//         createPage({
+//           path: `${node.frontmatter.slug}`,
+//           component: path.resolve('./src/components/Layouts/postLayout.tsx'),
+//           context: {
+//             slug: node.frontmatter.slug,
+//           },
+//         });
+//       });
+//       resolve();
+//     });
+//   });
+// };
