a59d9158d60d4cb70bf2b099f9c351bee0168284
Matt Zuckermann <mczuckermann@gmail.com>
2019-10-02 16:48:45 -0700

    Adjusted site context and updated Gatsby package

--- src/context/SiteContext.js
+++ src/context/SiteContext.js
@@ -1,26 +1,15 @@
 import React, { createContext } from 'react';
-import { ContentfulClient, ContentfulProvider } from 'react-contentful';
-
-// space: process.env.SPACE,
-// accessToken: process.env.ACCESS_TOKEN,
-
-const contentfulClient = new ContentfulClient({
-  space: "Josh's Website Content",
-  accessToken: 'rf82jp4px8qn',
-});
 
 const defaultValues = {
-  boolean: `this works`,
+  boolean: 'this works',
 };
 
 export const SiteContext = createContext(defaultValues);
 
 export const SiteProvider = ({ children }) => {
   return (
-    <ContentfulProvider client={contentfulClient}>
-      <SiteContext.Provider value={defaultValues}>
-        {children}
-      </SiteContext.Provider>
-    </ContentfulProvider>
+    <SiteContext.Provider value={defaultValues}>
+      {children}
+    </SiteContext.Provider>
   );
 };
