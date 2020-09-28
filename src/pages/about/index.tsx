8740986724c4e979e7d3380b2bf48328855d8082
Matt Zuckermann <mczuckermann@gmail.com>
2020-09-28 05:25:20 -0700

    Rid of unnecessary imports

--- src/pages/about/index.tsx
+++ src/pages/about/index.tsx
@@ -4,8 +4,6 @@ import Grid from '@material-ui/core/Grid';
 import YouTubeVideo from '../../components/YouTubeVideo';
 import { makeStyles } from '@material-ui/styles';
 import MobileStepper from '@material-ui/core/MobileStepper';
-import Paper from '@material-ui/core/Paper';
-import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 import SEO from '../../components/SEO';
 import classnames from 'classnames';
