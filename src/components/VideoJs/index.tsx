6908b4b0a57e958a02916ba5333bb10e8fc3c2de
Matt Zuckermann <mczuckermann@gmail.com>
2020-10-07 09:50:45 -0700

    Implemented VideoJS player with Mux video hosting

---
+++ src/components/VideoJs/index.tsx
@@ -0,0 +1,35 @@
+import React, { useEffect, useRef } from 'react';
+import videojs from 'video.js';
+import '../../../node_modules/video.js/dist/video-js.css';
+import '@videojs/themes/dist/forest/index.css';
+
+const VideoJs = props => {
+  const { videoSrc, videoPoster } = props;
+  const playerRef = useRef(null);
+
+  useEffect(() => {
+    const player = videojs(playerRef.current, {}, () => {
+      player.src(videoSrc);
+      player.on('ended', () => {
+        player.handleTechLoadStart_();
+        player.hasStarted(false);
+      });
+    });
+    return () => {
+      player.dispose();
+    };
+  }, []);
+
+  return (
+    <div data-vjs-player style={{ marginBottom: '25px', borderRadius: '35px' }}>
+      <video
+        ref={playerRef}
+        className=" video-js vjs-theme-forest vjs-16-9"
+        controls
+        poster={videoPoster}
+      />
+    </div>
+  );
+};
+
+export default VideoJs;
--- src/pages/about/index.tsx
+++ src/pages/about/index.tsx
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
 import { graphql, useStaticQuery } from 'gatsby';
 import Grid from '@material-ui/core/Grid';
-import YouTubeVideo from '../../components/YouTubeVideo';
 import { makeStyles } from '@material-ui/styles';
 import MobileStepper from '@material-ui/core/MobileStepper';
 import Button from '@material-ui/core/Button';
@@ -10,6 +9,7 @@ import classnames from 'classnames';
 import navigator from '../../js/navigator';
 import './about.css';
 import { useSpring, animated } from 'react-spring';
+import VideoJs from '../../components/VideoJs';
 
 const useStyles = makeStyles({
   root: {
@@ -35,9 +35,11 @@ const useStyles = makeStyles({
   },
   videoPlaybackWrapper: {
     flexGrow: 1,
+    marginRight: 'auto',
+    marginLeft: 'auto',
     '@media(max-width: 959px)': {
       textAlign: 'center',
-      width: '100%',
+      width: '90%',
     },
   },
   backgroundVideo: {
@@ -69,7 +71,7 @@ const useStyles = makeStyles({
     height: '5px',
     marginLeft: '5px',
     marginRight: '5px',
-    backgroundColor: 'black',
+    backgroundColor: 'white',
     '@media(max-width: 959px)': {
       marginLeft: '30px',
       marginRight: '30px',
@@ -86,12 +88,31 @@ const useStyles = makeStyles({
     width: '410px',
     height: '770px',
     overflow: 'hidden',
+    '@media(max-width: 959px)': {
+      width: '785px',
+    },
+    '@media(max-width: 765px)': {
+      width: '410px',
+    },
   },
   iframePhone: {
     marginBottom: '0px',
     width: '410px',
     height: '1550px',
     overflow: 'hidden',
+    '@media(max-width: 480px)': {
+      width: '100%',
+      height: '1470px',
+    },
+    '@media(max-width: 380px)': {
+      height: '1430px',
+    },
+    '@media(max-width: 360px)': {
+      height: '1410px',
+    },
+    '@media(max-width: 340px)': {
+      height: '1390px',
+    },
   },
 });
 
@@ -122,11 +143,28 @@ const About = () => {
 
   const [activeStep, setActiveStep] = useState(0);
   const maxSteps = allContentfulPhotoAlbum.edges[0].node.carouselImage.length;
-  const videoArray = [
-    'https://www.youtube.com/embed/nIds3reW_dY?rel=0',
-    'https://www.youtube.com/embed/bnRVlXlIsJs?rel=0',
-    'https://www.youtube.com/embed/7mK53nDB-Cw?rel=0',
+
+  const muxVideoArray = [
+    {
+      url:
+        'https://stream.mux.com/QBlTqHrlNASrg5m202LmYBR01ckD7UoByzRwj8KEcnIqw.m3u8',
+      thumbnail:
+        'https://res.cloudinary.com/dmgmf4lbz/image/upload/v1602074131/Video%20Files/Thumbnail%20Photos/Screen_Shot_2020-10-07_at_5.33.46_AM_lbp7gq.png',
+    },
+    {
+      url:
+        'https://stream.mux.com/6Sw5CWVPlXsO00KpxkTkLUA8WFYBEhMe4ucTBenNzoiM.m3u8',
+      thumbnail:
+        'https://res.cloudinary.com/dmgmf4lbz/image/upload/v1602073305/Video%20Files/Thumbnail%20Photos/Screen_Shot_2020-10-07_at_5.19.40_AM_uviw96.png',
+    },
+    {
+      url:
+        'https://stream.mux.com/7meQHJH001whheuTxXR6bs2RqcXVR1YI6ZeVpUZT7qXA.m3u8',
+      thumbnail:
+        'https://res.cloudinary.com/dmgmf4lbz/image/upload/v1602073601/Video%20Files/Thumbnail%20Photos/Screen_Shot_2020-10-07_at_5.25.54_AM_fqqtxt.png',
+    },
   ];
+
   const fade = useSpring({
     from: { opacity: 0 },
     opacity: 1,
@@ -155,7 +193,7 @@ const About = () => {
           `chicago`,
         ]}
       />
-      <Grid container spacing={1}>
+      <Grid container spacing={3}>
         <Grid
           style={{ paddingBottom: '0px' }}
           item
@@ -174,10 +212,16 @@ const About = () => {
               dangerouslySetInnerHTML={{ __html: markdownRemark.html }}
             />
           </Grid>
-          {videoArray.map(video => (
+          {muxVideoArray.map(video => (
             <Grid item lg={12} md={12} sm={12} xs={12}>
               <div className={classes.videoPlaybackWrapper}>
-                <YouTubeVideo videoLink={`${video}`} />
+                <VideoJs
+                  videoSrc={{
+                    src: video.url,
+                    type: 'application/x-mpegURL',
+                  }}
+                  videoPoster={video.thumbnail}
+                />
               </div>
             </Grid>
           ))}
@@ -192,7 +236,6 @@ const About = () => {
         >
           <div className={classes.root} style={{ position: 'relative' }}>
             <img
-              id="carouselImage"
               className={classes.img}
               src={
                 allContentfulPhotoAlbum.edges[0].node.carouselImage[activeStep]
@@ -233,14 +276,18 @@ const About = () => {
             }}
           >
             <iframe
+              ref={iframeRef}
+              id="iframe1"
+              border="0"
+              frameborder="0"
+              style={{ backgroundColor: 'black' }}
               scrolling="no"
               src="/iframes"
               // classnames package checking what device is rendering iFrame and responding
               // accordingly with proper CSS.
               className={classnames('', {
                 [classes.iframeTabletLaptop]:
-                  // !navigator() || (navigator() && screen.width >= 700),
-                  true,
+                  !navigator() || (navigator() && screen.width >= 700),
                 [classes.iframePhone]: navigator() && screen.width < 700,
               })}
             />
