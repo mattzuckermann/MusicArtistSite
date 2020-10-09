0ec4c086aa7d9cc8654582d99ea9c523df8cd356
Matt Zuckermann <mczuckermann@gmail.com>
2020-10-09 06:33:10 -0700

    Added credits to content producer's video and added conditional to check if window is fullscreen to avoid error for exiting fullscreen when not in fullscreen

--- src/components/VideoJs/index.tsx
+++ src/components/VideoJs/index.tsx
@@ -1,19 +1,23 @@
 import React, { useEffect, useRef } from 'react';
 import videojs from 'video.js';
+import { faYoutube } from '@fortawesome/free-brands-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import '../../../node_modules/video.js/dist/video-js.css';
 import '@videojs/themes/dist/forest/index.css';
 
 const VideoJs = props => {
-  const { videoSrc, videoPoster } = props;
+  const { videoSrc, videoPoster, videoCredits } = props;
   const playerRef = useRef(null);
 
   useEffect(() => {
     const player = videojs(playerRef.current, {}, () => {
       player.src(videoSrc);
       player.on('ended', () => {
-        player.handleTechLoadStart_();
         player.hasStarted(false);
-        player.exitFullscreen();
+        if((window.fullScreen) || (window.innerWidth == screen.width && window.innerHeight == screen.height)) {
+          player.exitFullscreen();
+        }
+        player.handleTechLoadStart_();
       });
     });
     return () => {
@@ -22,13 +26,50 @@ const VideoJs = props => {
   }, []);
 
   return (
-    <div data-vjs-player style={{ marginBottom: '25px', borderRadius: '35px' }}>
+    <div data-vjs-player style={{ marginBottom: '40px', borderRadius: '35px' }}>
       <video
         ref={playerRef}
         className=" video-js vjs-theme-forest vjs-16-9"
         controls
         poster={videoPoster}
       />
+      <div style={{ marginTop: '3px', padding: 'auto 0px auto 0px' }}>
+        {videoCredits.length != 0 && (
+          <caption
+            style={{
+              float: 'left',
+              marginLeft: '10px',
+              fontSize: '13px',
+              backgroundColor: 'rgba(0,0,0,0.1)',
+            }}
+          >
+            credits:{' '}
+            <a
+              rel="noopener noreferrer"
+              target="_blank"
+              href={videoCredits[0].portfolio}
+              title="Portfolio"
+            >
+              {videoCredits[0].name}
+            </a>{' '}
+            <a
+              rel="noopener noreferrer"
+              target="_blank"
+              href={videoCredits[0].youtube}
+              title="YouTube Channel"
+              >
+              <FontAwesomeIcon
+                style={{
+                  fontSize: '18px',
+                  color: '#f70103',
+                  padding: '2px 0px 0px 0px',
+                }}
+                icon={faYoutube}
+              />
+            </a>
+          </caption>
+        )}
+      </div>
     </div>
   );
 };
