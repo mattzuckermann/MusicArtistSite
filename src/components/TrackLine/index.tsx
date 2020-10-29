92aaa710176a5d9d96c300aa930a97fe0792e239
Matt Zuckermann <mczuckermann@gmail.com>
2020-10-29 08:41:23 -0700

    made TrackLine play button responsive to hover/touch

--- src/components/TrackLine/index.tsx
+++ src/components/TrackLine/index.tsx
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import classNames from 'classnames';
+import navigator from '../../js/navigator'
 
 const TrackLine = ({
   playPauseIcons,
@@ -17,16 +18,48 @@ const TrackLine = ({
   const seconds = Math.floor(track.cloudinaryAudio[0].duration % 60);
   const secondsFormatted = seconds === 0 ? '00' : seconds < 10 ? `0${seconds}` : seconds;
   const trackDuration = `${minutesFormatted}:${secondsFormatted}`;
+
+  const [sourceObject, setSourceObject] = useState(playPauseIcons[0]);
+  const [isHovered, setIsHovered] = useState(false);
+  const [isTouched, setIsTouched] = useState(false);
+  const [isMouseDown, setIsMouseDown] = useState(false)
+
+  useEffect(() => {
+    if (!navigator()) {
+      if (currentTrack === index && playing) setSourceObject(soundIcons[1]);
+      else if (currentTrack === index && !playing) setSourceObject(soundIcons[0]);
+      else if (isHovered && isMouseDown) setSourceObject(playPauseIcons[2])
+      else if (isHovered) setSourceObject(playPauseIcons[1]);
+      else setSourceObject(playPauseIcons[0]);
+    } else {
+      if (currentTrack === index && playing) setSourceObject(soundIcons[1]);
+      else if (currentTrack === index && !playing) setSourceObject(soundIcons[0]);
+      else if (isTouched) setSourceObject(playPauseIcons[0]);
+      else setSourceObject(playPauseIcons[1]);
+    }
+  }, [currentTrack, playing, isHovered, isTouched, isMouseDown]);
+
   return (
-    <div style={{ margin: "5px 0px"}}>
+    <div style={{ margin: "0px 0px"}}>
         <img
-          src={
-            currentTrack === index && playing
-            ? soundIcons[1].node.secure_url
-            : currentTrack === index
-              ? soundIcons[0].node.secure_url
-              : playPauseIcons[0].node.secure_url
-          }
+          src={sourceObject.node.secure_url}
+          onMouseEnter={() => {
+            if (!navigator()) setIsHovered(true);
+          }}
+          onMouseLeave={() => {
+            if (!navigator()) {
+              setIsHovered(false);
+              setIsMouseDown(false);
+            }
+          }}
+          onMouseDown={() => {
+            if (!navigator()) setIsMouseDown(true);
+          }}
+          onMouseUp={() => {
+            if (!navigator()) setIsMouseDown(false);
+          }}
+          onTouchStart={() => setIsTouched(true)}
+          onTouchEnd={() => setIsTouched(false)}
           style={{
             width: '25x',
             height: '25px',
